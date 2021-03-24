0a9bd502d8a504ed6b918e0bac0c49f4a348c04b
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2021-03-23 23:13:08 -0300

    Contextos e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Modal from 'react-modal';
 
 import { GlobalStyle } from "./styles/global";
 
+import { TransactionsProvider } from './hooks/useTransactions';
+
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from './components/NewTransactionModal';
@@ -21,7 +23,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -31,6 +33,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
